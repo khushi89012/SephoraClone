e0da73629b8b6c1e40d1296579a2fdcaddd67dc7
Ritu1011 <bankeyritu@gmail.com>
2022-05-06 21:30:46 +0530

    done
    done

--- frontend/nav-footer/src/App.jsx
+++ frontend/nav-footer/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+// import { useState } from 'react'
+// import logo from './logo.svg'
 import './App.css'
 import {Header } from './components/Header'
 import { Footer } from './components/Footer'
