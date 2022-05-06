88faa4f991c7b8f99a5bce3ca7d81bcd115241ab
Ritu1011 <bankeyritu@gmail.com>
2022-05-06 12:57:09 +0530

    nav and footer
    nav and footer

---
+++ frontend/nav-footer/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Footer } from './components/Footer'
+import { Navbar } from './components/navbar'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Navbar/>
+
+     <Footer/>
+     
+    </div>
+  )
+}
+
+export default App
