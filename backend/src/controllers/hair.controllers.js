b291ba03117ece87ab289360a2421327fd822853
khushi Gupta <95909861+khushi89012@users.noreply.github.com>
2022-05-08 16:58:51 +0530

    FINAL LOGIN ADDED

--- backend/src/controllers/hair.controllers.js
+++ backend/src/controllers/hair.controllers.js
@@ -3,7 +3,6 @@ const express=require("express");
 const Hair=require("../models/hair.models");
 
 const router=express.Router();
-
 router.get("",async (req,res)=>{
 try {
     const hair=await Hair.find().lean().exec();
@@ -13,12 +12,4 @@ try {
 }
 })
 
-// router.get("/:id",async (req,res)=>{
-//     try {
-//         const women=await Women.findById(req.params.id).lean().exec();
-//         return res.status(200).send(women);
-//     } catch (error) {
-//         return res.status(501).send({message:error.message})
-//     }
-//     })
 module.exports= router;
