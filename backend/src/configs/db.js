2378633ee75b6615af3dee02e3fc3dc5c62e02f7
Viraj Gupta <vgupta2@q3infotech.net>
2023-09-03 23:50:34 +0530

    fix: hide secret sources

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 module.exports=()=>{
-    return mongoose.connect("mongodb+srv://pappu:pappu123@cluster0.yswkt.mongodb.net/sephoraproject",{
+    return mongoose.connect(`mongodb+srv://${env.SECRET_KEY}@cluster0.yswkt.mongodb.net/sephoraproject`,{
         useNewUrlParser:true,
         useUnifiedTopology:true,
     }).then(()=>{
